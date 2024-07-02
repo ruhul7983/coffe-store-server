2bce27d562e22b4f6bb260345d2d628ad66c54eb
Ruhul Amin <ruhulamin7983@gmail.com>
2024-07-02 12:07:35 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,140 @@
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
+const express = require("express");
+require('dotenv').config()
+const cors = require('cors');
+const app = express();
+//middle wire
+app.use(cors());
+app.use(express.json());
+
+
+// port 
+const port = process.env.PORT || 5000;
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rsqtl7q.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    const coffeeCollection = client.db("coffeeDB").collection('coffee');
+    const userCollection = client.db("coffeeDB").collection('user');
+    app.post("/coffee", async (req, res) => {
+      const coffee = req.body;
+      console.log(coffee);
+      const result = await coffeeCollection.insertOne(coffee);
+      res.send(result);
+
+    })
+
+    app.get("/coffee", async (req, res) => {
+      const cursor = coffeeCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+    app.get('/coffee/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await coffeeCollection.findOne(query);
+      res.send(result);
+    })
+
+    app.delete("/coffee/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await coffeeCollection.deleteOne(query);
+      res.send(result)
+    })
+
+
+    app.put('/coffee/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updatedCoffee = req.body;
+
+      const coffee = {
+        $set: {
+          name: updatedCoffee.name,
+          chef: updatedCoffee.chef,
+          supplier: updatedCoffee.supplier,
+          taste: updatedCoffee.taste,
+          category: updatedCoffee.category,
+          details: updatedCoffee.details,
+          photoUrl: updatedCoffee.photoUrl,
+          quantity: updatedCoffee.quantity,
+        }
+      }
+
+      const result = await coffeeCollection.updateOne(filter, coffee, options);
+      res.send(result);
+
+    })
+
+    // User Collection
+
+    app.post("/user", async (req, res) => {
+      const user = req.body;
+      console.log(user);
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+
+    })
+
+    
+    app.get("/user", async (req, res) => {
+      const cursor = userCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+    app.delete("/user/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await userCollection.deleteOne(query);
+      res.send(result)
+    })
+
+    app.patch("/user",async(req,res)=>{
+      const user = req.body;
+      const filter = {email:user.email};
+      const updatedUser = {
+        $set:{
+          lastSignInTime:user.lastSignInTime,
+        }
+      } 
+      const result = await userCollection.updateOne(filter,updatedUser);
+      res.send(result);
+    })
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.get("/", (req, res) => {
+  res.send("Coffee server is running");
+})
+
+
+app.listen(port, () => {
+  console.log("App is running port ", port);
+})
